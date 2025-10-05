cc39ddb99609b4eced911146f900e11504b91dd5
AlexdeCampos <alexcampos.dev@gmail.com>
2025-10-05 12:39:03 -0300

    route to geretare the csrf token(use only for postman)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+//Rota criada unicamente para gerar token para o postman
+Route::get('/postman/csrf', function () {
+    return csrf_token();
+});
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
